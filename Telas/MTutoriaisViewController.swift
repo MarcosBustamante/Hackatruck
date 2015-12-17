40af4b89c9bd95e0bfe8ce9610ab62b3d8c08fcc
= <=>
2015-12-17 08:45:13 -0200

    Tela de meus tutoriais, segmented control e visualizacao da lista

--- Telas/MTutoriaisViewController.swift
+++ Telas/MTutoriaisViewController.swift
@@ -18,26 +18,26 @@ class MTutoriaisViewController: UIViewController, UITableViewDataSource, UITable
     var npublicados = [[String: String]]()
     var pedidos = [[String: String]]()
     
+    var data = [[[String: String]]]()
+    let titulos = ["Publicado", "Rascunho", "Pedido"]
+    
     
     override func viewDidLoad() {
-        
-        
+        super.viewDidLoad()
         self.tutoriais = Tutoriais.listarTutoriais()
         for var tutorial in self.tutoriais{
-            if tutorial["publicado"]=="1"{
+            if tutorial["publicado"] == "true"{
                 self.publicados.append(tutorial)
             }
             else{
                 npublicados.append(tutorial)
             }
         }
-            
+        
+        self.data = [self.publicados, self.npublicados, self.tutoriais]
         
         self.tableMT.dataSource = self
         self.tableMT.delegate = self
-        
-
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -45,59 +45,26 @@ class MTutoriaisViewController: UIViewController, UITableViewDataSource, UITable
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
-        if self.segmentAbas.selectedSegmentIndex == 0{
-            return self.publicados.count
-        }
-        if self.segmentAbas.selectedSegmentIndex == 1{
-            return self.npublicados.count
-        }
-        if self.segmentAbas.selectedSegmentIndex == 2{
-            return self.pedidos.count
-        }
-        
-        return tutoriais.count
+        return self.data[self.segmentAbas.selectedSegmentIndex].count
     }
     
    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let linhaAtual = indexPath.row
         let linha = tableView.dequeueReusableCellWithIdentifier("cellMT") as! MTuTableViewCell
         
-        
-        if self.segmentAbas.selectedSegmentIndex == 0{
-            
-            let tutorial =  publicados[linhaAtual]
-            
-            linha.labelNomeTutorial.text = tutorial["nome_tutorial"]
-            linha.labelPublicado.text = "Publicado"
-            return linha
-        }
-        if self.segmentAbas.selectedSegmentIndex == 1{
-            
-            let tutorial =  npublicados[linhaAtual]
-            
-            linha.labelNomeTutorial.text = tutorial["nome_tutorial"]
-            linha.labelPublicado.text = "Rascunho"
-            return linha
-        }
-        if self.segmentAbas.selectedSegmentIndex == 2{
-            
-            let tutorial =  pedidos[linhaAtual]
-            
-            linha.labelNomeTutorial.text = tutorial["nome_tutorial"]
-            linha.labelPublicado.text = "Pedido"
-            return linha
-        }
+        let data = self.data[self.segmentAbas.selectedSegmentIndex][linhaAtual]
+        linha.labelNomeTutorial.text = data["nome_tutorial"]
+        linha.labelPublicado.text = self.titulos[self.segmentAbas.selectedSegmentIndex]
         
         return linha
+        
     }
     
     @IBAction func indexChanged(sender: AnyObject) {
-        
-       
-        
+        self.tableMT.reloadData()
     }
     
 
